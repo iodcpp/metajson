f54b6501d51864c9bd186056ce5d92d9a5225813
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2017-06-04 16:07:55 +0200

    Work on single header

--- make_single_header.py
+++ make_single_header.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import tempfile
 import subprocess
 import os,re,sys,shutil
@@ -15,20 +17,22 @@ def process_file(f, processed, output):
             process_file(join(install_dir, "include", m.group(1)), processed, output)
         elif re.match("#pragma once", line):
             pass
-        elif re.match("namespace\s*iod\s*", line):
-            output.write("namespace metajson")
+        elif re.match("namespace\s*iod(\s*)", line):
+            m2 = re.match("namespace\s*iod(\s*)", line)
+            output.append("namespace metajson" + m2.group(1))
         else:
-            output.write(line)
+            output.append(line)
 
 if __name__ == "__main__":
 
     if os.path.exists(sys.argv[1]):
         print(sys.argv[1], "already exists")
         exit(1)
-
-    output=open(sys.argv[1], 'w')
     
-    # create temp directory.
+
+    output_file=open(sys.argv[1], 'w')
+
+        # create temp directory.
     tmp_dir=tempfile.mkdtemp()
     src_dir=join(tmp_dir, "src")
     build_dir=join(tmp_dir, "build")
@@ -51,8 +55,29 @@ if __name__ == "__main__":
     subprocess.check_call(["make", "install", "-j4"])
 
     # Generate single file header.
-    process_file(join(install_dir, "include/iod/metajson/metajson.hh"), processed, output)
+    lines=[]
+    process_file(join(install_dir, "include/iod/metajson/metajson.hh"), processed, lines)
 
+    body=[]
+    includes=[]
+    for line in lines:
+        m = re.match("^\s*#include.*$", line)
+        if m:
+            includes.append(line)
+        else:
+            body.append(line)
+
+    output_file.write("// Single header version of iod::metajson [1].\n")
+    output_file.write("// This file is generated, do not edit it directly.\n")
+    output_file.write("// [1] https://github.com/iodcpp/metajson\n\n")
+    output_file.write("#pragma once\n\n")
+    for l in includes:
+        output_file.write(l)
+    output_file.write("\n\n")
+    for l in body:
+        output_file.write(l)
+    output_file.close()
+    
     print("metajson single header", sys.argv[1], "generated.")
     # Cleanup
     shutil.rmtree(tmp_dir)
